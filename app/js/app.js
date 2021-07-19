2954f7a7626d040536e91f1cc81cbc950a87c097
Felipe Coutinho <philipecout@gmail.com>
2021-07-19 10:08:43 -0300

    first commit[

---
+++ app/js/app.js
@@ -0,0 +1,4 @@
+const negociacao = new Negociacao(new Date(), 1, 500);
+
+console.log(negociacao);
+console.log('oi');
---
+++ app/js/models/negociacao.js
@@ -0,0 +1,21 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade;
+        this._valor = valor;
+        console.log('dfd');
+    }
+
+    get data() {
+        return this.data;
+    }
+
+    get quantidade() {
+        return this.quantidadeç
+    }
+
+    get valor() {
+        return this.valor;
+    }
+
+}
